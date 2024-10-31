5d53134632418611979bef1dae80f01ef05af8ef
chamindawijayasundara <cwijayasundara@gmail.com>
2024-10-31 06:57:03 +0000

    refactored the form retreaver

--- claim_processor/claim_form_retriver.py
+++ claim_processor/claim_form_retriver.py
@@ -22,23 +22,35 @@ embed_model = OpenAIEmbedding(model="text-embedding-3-large")
 llm = OpenAI("gpt-4o")
 
 Settings.llm = llm
+
 Settings.embed_model = embed_model
 
 ctx = StorageContext.from_defaults(persist_dir=claim_form_store_location)
+
 index = load_index_from_storage(ctx)
 
 query_engine = index.as_query_engine()
 
-response = query_engine.query(
-    "Who filed the insurance claim for the accident that happened on Sunset Blvd?"
-)
-print((str(response)))
+def get_response_form_store_chat_engine(query):
+    chat_engine = index.as_chat_engine()
+    response = chat_engine.chat(query)
+    return str(response)
 
-response = query_engine.query("How did Ms. Patel's accident happen?")
-print((str(response)))
+def get_response_form_store_query_engine(query):
+    response = query_engine.query(query)
+    return str(response)
 
-chat_engine = index.as_chat_engine()
-response = chat_engine.chat(
-    "Given the accident that happened on Lombard Street, name a party that is liable for the damages and explain why."
-)
-print((str(response)))
+
+query_1 = "Who filed the insurance claim for the accident that happened on Sunset Blvd?"
+query_2 = "How did Ms. Patel's accident happen?"
+
+response_1 = get_response_form_store_query_engine(query_1)
+response_2 = get_response_form_store_query_engine(query_2)
+
+print(response_1)
+print(response_2)
+
+response_3 = get_response_form_store_chat_engine(query_1)
+print(response_3)
+response_4 = get_response_form_store_chat_engine(query_2)
+print(response_4)
